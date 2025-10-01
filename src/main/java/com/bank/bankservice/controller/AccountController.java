2cff5f40f8b66dc8da249a4fb464584b30e3b7e6
rewealwarrior-web <reweal.warrior@gmail.com>
2025-10-02 00:39:35 +0400

    Update AccountController.java
    Убран избыточный код

--- src/main/java/com/bank/bankservice/controller/AccountController.java
+++ src/main/java/com/bank/bankservice/controller/AccountController.java
@@ -33,7 +33,7 @@ import lombok.RequiredArgsConstructor;
 
 
 
-@RestController
+@RestController //Используя RestController у нас есть возможность не оборачивать DTO в ResponseEntity и не писать аннотацию ResponseBody над исходящими DTO
 @RequestMapping("api/accounts")
 @RequiredArgsConstructor
 @Tag(name = "Bank logic")
@@ -45,7 +45,7 @@ public class AccountController {
     @Operation(summary = "Creating a bank account")
     @VerifyCreatingAccess
     @PostMapping
-    public ResponseEntity<AccountDto> create(@RequestBody @Valid RequestCreateAccount requestCreateAccount) {
+    public AccountDto create(@RequestBody @Valid RequestCreateAccount requestCreateAccount) {
 
         AccountDto accountDto = accountCreationService.create(AccountDto.builder()
                                                                         .id(null)
@@ -54,59 +54,59 @@ public class AccountController {
                                                                         .accountType(requestCreateAccount.getAccountType())
                                                                         .balance(new BigDecimal(0))
                                                                         .build());
-        return ResponseEntity.ok(accountDto);
+        return accountDto;
     } 
 
     @Operation(summary = "Getting a specific user's bank account")
     @VerifyListningAccess
     @GetMapping("/{number}")
-    public ResponseEntity<AccountDto> getAccount(@RequestParam Long userId, @PathVariable Long number) {
+    public AccountDto getAccount(@RequestParam Long userId, @PathVariable Long number) {
 
         AccountDto accountDto = accountListingService.getUserAccount(userId, number);
-        return ResponseEntity.ok(accountDto);
+        return accountDto;
     }
 
     @Operation(summary = "Getting all bank accounts of a specific user")
     @VerifyListningAccess
     @GetMapping
-    public ResponseEntity<List<AccountDto>> getAccounts(@RequestParam Long userId) {
+    public List<AccountDto> getAccounts(@RequestParam Long userId) {
 
         List<AccountDto> accountsDto = accountListingService.getUserAccounts(userId);
-        return ResponseEntity.ok(accountsDto);
+        return accountsDto;
     }
 
     @Operation(summary = "Getting accounts of a certain type of a specific user")
     @VerifyListningAccess
     @GetMapping("/types/{accountType}")
-    public ResponseEntity<List<AccountDto>> getByType(@RequestParam Long userId, @PathVariable AccountType accountType) {
+    public List<AccountDto> getByType(@RequestParam Long userId, @PathVariable AccountType accountType) {
 
         List<AccountDto> accountsDto = accountListingService.getUserAccountsByType(userId, accountType);
-        return ResponseEntity.ok(accountsDto);
+        return accountsDto;
     }
 
     @Operation(summary = "Adding money to a specific bank account")
     @PostMapping("/{payeeNumber}/deposits")
-    public ResponseEntity<DepositResponse> deposit(@PathVariable Long payeeNumber, @RequestBody BigDecimal amount) {
+    public DepositResponse deposit(@PathVariable Long payeeNumber, @RequestBody BigDecimal amount) {
 
         DepositResponse depositResponse = accountBalanceManipulationService.deposit(payeeNumber, amount);
-        return ResponseEntity.ok(depositResponse);
+        return depositResponse;
     }
 
     @Operation(summary = "Debiting money from specific bank account")
     @VerifyBalanceManipulationAccess
     @PostMapping("/{sourceMoneyNumber}/withdrawals")
-    public ResponseEntity<WithdrawResponse> withdraw(@PathVariable Long sourceMoneyNumber, @RequestBody BigDecimal amount) {
+    public WithdrawResponse withdraw(@PathVariable Long sourceMoneyNumber, @RequestBody BigDecimal amount) {
 
         WithdrawResponse withdrawResponse = accountBalanceManipulationService.withdraw(sourceMoneyNumber, amount);
-        return ResponseEntity.ok(withdrawResponse);
+        return withdrawResponse;
     }
 
     @Operation(summary = "Trasfering money from one bank account to another")
     @VerifyBalanceManipulationAccess
     @PostMapping("/transfers")
-    public ResponseEntity<TransferResponse> transfer(@RequestBody TransferRequest transferRequest) {
+    public TransferResponse transfer(@RequestBody TransferRequest transferRequest) {
         
         TransferResponse transferResponse = accountBalanceManipulationService.transfer(transferRequest);
-        return ResponseEntity.ok(transferResponse);
+        return transferResponse;
     }
 }
