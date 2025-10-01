f2139e9a0f8954cad35017c75512c91cceed9c9a
rewealwarrior-web <reweal.warrior@gmail.com>
2025-10-02 00:52:31 +0400

    Update AccountRepository.java
    комментарии к коду

--- src/main/java/com/bank/bankservice/repository/AccountRepository.java
+++ src/main/java/com/bank/bankservice/repository/AccountRepository.java
@@ -8,7 +8,7 @@ import com.bank.bankservice.domain.model.Account;
 
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    List<Account> findByUserId(Long clientId);
+    List<Account> findByUserId(Long clientId);  //метод называется userId, а ищет по некому clientId - не совсем понятно.
 
-    Account findByNumber(Long number);
+    Account findByNumber(Long number); //Поизучай Optional - очень удобный инструмент, часто используемый на настоящих проектах.
 }
